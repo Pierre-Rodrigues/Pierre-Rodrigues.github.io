f49e037b2353862b96dd272212d3bb5cab6cad2d
P1errO <bourriket7@gmail.com>
2022-09-28 19:15:23 +0200

    animation buton accordion

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,6 @@ let toggle = document.querySelector(".toggle");
 let body = document.querySelector("body");
 
 toggle.addEventListener("click", function() {
+    console.log("oui");
     body.classList.toggle('open')
-}) 
+});
